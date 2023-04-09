9c78fc61cf665169fa838b408c9aca935022f427
Márcio Vinícius Rodrigues da Silva <vinisilva02@hotmail.com>
2023-04-09 16:22:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ControleDeContas.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("FinanceiroDbConnection");
+builder.Services.AddDbContext<FinanceiroContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "Conta",
+    pattern: "{controller=Conta}/{action=Index}/{id?}");
+
+app.Run();
